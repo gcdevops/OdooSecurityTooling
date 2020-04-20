e641f50efeb7aca66656a614a20174a4bce6ddd2
Eric Wu <e-wu@users.noreply.github.com>
2020-04-20 09:26:00 -0400

    Update and create report directory.

--- docker-security/anchore/scan.sh
+++ docker-security/anchore/scan.sh
@@ -1,3 +1,12 @@
 . ./set-env.sh
 
+# Pull down latest feed
+anchore-cli system feeds list
+anchore-cli system wait
+
+# Scan Odoo version being used
 anchore-cli image add odoo:13.0
+anchore-cli image wait  odoo:13.0
+
+# Get the list of vulnerabilities
+anchore-cli image vuln odoo:13.0 os > ../../reports/acnhore.txt
