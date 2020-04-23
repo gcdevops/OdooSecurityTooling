331f3714c634b146e819ec4ce100bd8c42199b27
Eric Wu <e-wu@users.noreply.github.com>
2020-04-23 07:45:29 -0400

    Move installation on anchore files out of home dir

--- docker-security/anchore/install.sh
+++ docker-security/anchore/install.sh
@@ -1,16 +1,18 @@
 sudo -v
 
-# sudo apt-get update
-# sudo apt-get install python-pip
-# pip install anchorecli
+sudo apt-get update
+sudo apt-get install -y python-pip
+pip install anchorecli
 
-mkdir ~/aevolume
-cd ~/aevolume
+mkdir aevolume
+cd aevolume
 
 docker pull docker.io/anchore/anchore-engine:latest
 docker create --name ae docker.io/anchore/anchore-engine:latest
-docker cp ae:/docker-compose.yaml ~/aevolume/docker-compose.yaml
+docker cp ae:/docker-compose.yaml docker-compose.yaml
 docker rm ae
 
 docker-compose pull
 docker-compose up -d
+
+cd ..
